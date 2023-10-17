6723fdca5deda7c2d5da1da91d62455df5a59961
TYNIP <130399006+TYNIP@users.noreply.github.com>
2023-10-17 00:15:37 -0600

    Test pt1

--- test/SearchBar.test.js
+++ test/SearchBar.test.js
@@ -1,27 +1,23 @@
 // test/SearchBar.test.js
 const { expect } = require('chai');
-const SearchBar = require('../src/components/SearchBar'); // Import your component
+const SearchBar = require('../src/components/SearchBar'); // Import component
 
 describe('SearchBar Component', () => {
   it('should render a SearchBar', () => {
-    const searchBar = new SearchBar(); // Instantiate your component
+    const searchBar = new SearchBar(); // Instantiate component
 
-    // Use Chai assertions to check if the component renders as expected
+    // Check if the component renders as expected (CHAI)
     expect(searchBar.render()).to.include('Enter A Song Title');
     expect(searchBar.render()).to.include('Search');
   });
 
   it('should call the onSearch function when the button is clicked', () => {
     const onSearch = (value) => {
-      // Your custom implementation to track if the function was called
+      
     };
 
-    const searchBar = new SearchBar({ onSearch }); // Pass a mock function as a prop
-
-    // Simulate a button click
+    const searchBar = new SearchBar({ onSearch }); 
     searchBar.onButtonClick();
 
-    // Check if the onSearch function was called as expected
-    // Implement your specific expectations here
   });
 });
