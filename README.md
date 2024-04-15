6ef61717d2a9ce1e242e54086bb812ac1fd31133
TYNIP <130399006+TYNIP@users.noreply.github.com>
2024-04-14 22:29:27 -0600

    fixed app connection

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 SpotyJam is a React app that uses the Spotify API to give users the ability to manage their playlists with ease. With SpotyJam, you can search for tracks, create new playlists, add or remove songs, and update your existing playlists, in a faster and easy way.
 
+Website: [SpotyJam](https://spotyjam.netlify.app/) 
+
 ## Features
 
 - **Authentication**: SpotyJam authenticates the user by asking for access to their Spotify account. This is required for managing playlists effectively.
@@ -14,61 +16,44 @@ SpotyJam is a React app that uses the Spotify API to give users the ability to m
 
 - **Playlist Status**: This component displays all your playlists. You can select an existing playlist to view and manage its songs or create a new one.
 
-## Getting Started
-
-1. Clone this repository to your local machine:
-
-   ```shell
-   git clone https://github.com/TYNIP/SpotyJam_React_App.git
-   cd SpotyJam_React_App
 # How to Use
 
 SpotyJam is designed to be a user-friendly application that allows you to manage your Spotify playlists effortlessly. Follow these steps to get started and make the most of the app:
 
 1. **Authentication**:
-
    - When you first access SpotyJam, you will be prompted to authenticate your Spotify account.
    - Click the "Log In" button and follow the instructions to log in to your Spotify account.
    - You will be asked to grant access to your account, specifically for managing playlists. This access is necessary for the app to function properly.
 
 2. **Search for Tracks**:
-
    - Use the "Search Bar" component to find your favorite tracks. Simply enter a song title or related keywords.
    - Click the "Search" button to initiate the search.
 
 3. **View Search Results**:
-
    - The "Search Results" component will display the tracks that match your search criteria.
    - You can browse through the results and find the songs you want to add to your playlists.
 
 4. **Manage Playlists**:
-
    - The "Playlist Status" component lists all your playlists. Select an existing playlist by clicking on it or create a new one.
    - To create a new playlist, click the "Create New Playlist" button.
    - To view and manage the songs in a playlist, select the playlist from the list.
 
 5. **Add Songs to Playlists**:
-
    - When viewing search results, click the "+" button next to a track to add it to the currently selected playlist.
    - The selected playlist's status in the "Playlist" component will be updated to include the new songs.
 
 6. **Remove Songs from Playlists**:
-
    - In the "Playlist" component, you can remove songs from the selected playlist by clicking the "-" button next to the track you want to remove.
 
 7. **Save Your Playlists**:
-
    - To save your playlists on Spotify, click the "SAVE TO SPOTIFY" button in the "Playlist" component. This will persist your changes on Spotify.
 
 8. **Explore and Enjoy**:
-
    - Continue to search for new tracks, create and modify playlists, and enjoy managing your Spotify music collection.
 
-That's it! SpotyJam empowers you to organize and enjoy your music on Spotify with ease.
 
 ## Considerations
 - Tests are not currently supported.
-- Project under development.
 - Project with academic and practical purposes.
 - Project does not store any kind of data.
 
--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,52 +1,42 @@
 /* SETUP */
-const clientId = ''; //Spotify Clien Id
-const redirectUri = 'http://localhost:3000/'; //Accepted spotify redirect URI
+const clientId =  process.env.CLIENT_KEY;
+const redirectUri = 'https://spotyjam.netlify.app/'; 
 let accessToken; //Spotify Acces Token
 
 /* General function */
 const SpotifyApi = {
-    /* GET ACCES TOKEN status(if it is valid or it has expired) */
     async getAccessToken(){
-        //If an access token already exist, return it.
         if (accessToken){return accessToken};
-
-        //Get token if there is not already one
-            //Check if the access token and expiration time are in the current url 
             const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
             const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
             if (accessTokenMatch && expiresInMatch){
-                accessToken = accessTokenMatch[1]; //Extract and access ONLY token from url 
-
-                //Remove the acces token from the url by updating history entry
+                accessToken = accessTokenMatch[1]; 
                 window.history.pushState('Access Token', null, '/');
 
                 return accessToken;
             } else {
                 const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
-                window.location = accessUrl; // Redirect the user to Spotify's authorization page.
+                window.location = accessUrl; 
             }
     },
 
-    /* Ask and get data from Api | Search component connection*/
     async search(term){
-        const accessToken = await SpotifyApi.getAccessToken(); //Get access, valid token already checked by prev function
+        const accessToken = await SpotifyApi.getAccessToken();
 
-        //Request Spotify search endpoint to search tracks 
-                                        /* End Point */
+        /* End Point */
         const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             /* Authorisation */
             headers:{
-                Authorization: `Bearer ${accessToken}`, //Access token in the request headers.
+                Authorization: `Bearer ${accessToken}`, 
             },
         });
 
         //Parse response to JSON 
         const jsonResponse = await response.json();
-        if (!jsonResponse.tracks){ //If there are no tracks return an empty array
+        if (!jsonResponse.tracks){ 
             return [];
         } else {
-            //If there is data Map the track data to a simplified format and return it
             return jsonResponse.tracks.items.map(track =>({
                 id: track.id,
                 name: track.name,
@@ -59,39 +49,32 @@ const SpotifyApi = {
 
     /* ACCESS USER ACOUNT| POST playlist, and post access and changes by adding songs*/
     async savePlaylist(name, trackUris){
-        // If the name or track URIs are missing, do nothing and return.
         if (!name && !!trackUris.length){
             return;
         }
 
-        //Validate tokens for ensuring correct access and optimal post requests of playlist
-        const accessToken = await SpotifyApi.getAccessToken();  // Get valid access token
-        const headers = { Authorization: `Bearer ${accessToken}` }; //Access token in the request headers.
+        const accessToken = await SpotifyApi.getAccessToken();  
+        const headers = { Authorization: `Bearer ${accessToken}` }; 
         let userId;
 
-        //Get users ID by sending a request to Spotify's "me" endpoint
+
         const userResponse = await fetch('https://api.spotify.com/v1/me',{ headers });
         const userJsonResponse = await userResponse.json();
         userId = userJsonResponse.id;
 
-        // Create a new playlist by sending a POST request to the user's playlists endpoint with the user id
         const playlistResponse = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,{
             headers,
             method: 'POST',
-            body: JSON.stringify({name:name}), // Include the playlist name in the request body.
+            body: JSON.stringify({name:name}), 
         });
 
-        //Parse response to json
         const playlistJsonResponse = await playlistResponse.json();
-        
-        // Extract the ID of the created playlist for final post of tracks
         const playlistId = playlistJsonResponse.id;
 
-        // Add tracks to the newly created playlist by sending a POST request to the playlist's tracks endpoint.
         await fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,{
             headers,
             method: 'POST',
-            body: JSON.stringify({uris:trackUris}), // Include the track URIs in the request body.
+            body: JSON.stringify({uris:trackUris}), 
         });
 
         alert("Playlist saved successfully");
